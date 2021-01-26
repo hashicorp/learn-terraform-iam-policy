89cfb7121adc8a0a66780dcda5d6bffc36345228
Rachel Sharp <rachel@hashicorp.com>
2021-01-25 23:21:38 -0500

    main config

--- main.tf
+++ main.tf
@@ -11,57 +11,35 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "test-bucket-${uuid()}"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-data "aws_iam_policy_document" "example" {
-  statement {
-    actions   = ["s3:ListAllMyBuckets"]
-    resources = ["arn:aws:s3:::*"]
-  }
-  statement {
-    actions   = ["s3:*"]
-    resources = [aws_s3_bucket.bucket.arn]
-  }
-}
-
 
 resource "aws_iam_policy" "policy" {
-  name        = "test_policy_${uuid()}"
+  name        = "test_policy"
   description = "My test policy"
 
-  policy = data.aws_iam_policy_document.example.json
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:ListAllMyBuckets"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+
+  ]
+"Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.bucket.arn}"
+    }
+
+  ]
 
 }
-resource "aws_iam_user" "new_user" {
-  name = "new_user"
-}
-
-resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "test-attachment"
-  policy_arn = aws_iam_policy.policy.arn
-  users      = [aws_iam_user.new_user.name]
+EOT
 }
